70fa6c89635b5e14e9e5b1a5da38d92f649ded18
Doan Ngoc The Vinh <vinhdoanngocthe@gmail.com>
2021-09-27 23:45:29 +0700

    Init new Automation Test Framework

---
+++ testcases/com/bankguru/payment/Payment_02_Deposit.java
@@ -0,0 +1,20 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Payment_02_Deposit {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void Name() {
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+	}
+}
